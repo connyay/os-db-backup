722cd741200abd8105c0251b7c718a1906000eba
Connor Hindley <conn.hindley@gmail.com>
2014-12-21 11:29:32 -0500

    use a time stamped tmp directory

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 'use strict';
+var fs = require('fs');
 var knox = require('knox');
 
 var OpenShiftDB = function(options) {
     this.client = knox.createClient(options);
+    var d = new Date();
+    this.timestamp = options.timestamp || (d.toISOString());
 };
 
 function has(db) {
     var DB = db.toUpperCase();
-    return (typeof process.env['OPENSHIFT_' + DB + 'DB_HOST']);
+    return (typeof process.env['OPENSHIFT_' + DB + 'DB_HOST'] !== 'undefined');
 }
 
 OpenShiftDB.prototype.backup = function() {
+    this._createTmpDir();
     this._action('backup');
 };
 
@@ -28,6 +32,10 @@ OpenShiftDB.prototype._action = function(action) {
     if (has('mongodb')) {
         require('./lib/mongodb')[action]();
     }
+};
+
+OpenShiftDB.prototype._createTmpDir = function() {
+    fs.mkdirSync('/tmp/' + this.timestamp);
 };
 
 
